888ff5bbedc2370784562811a8dd25301cf81a8a
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2021-12-26 18:34:40 +0530

    (add) parent components for pages

---
+++ components/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Bio from "../components/Bio";
+
+function Home() {
+	return <Bio />
+}
+
+export default Home;
---
+++ components/Posts.js
@@ -0,0 +1,8 @@
+import React from "react";
+import BlogHero from "../components/BlogHero";
+
+function Posts() {
+	return <BlogHero />
+}
+
+export default Posts;
