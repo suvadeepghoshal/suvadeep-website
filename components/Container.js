ead7d8b1612b25108fc5ace1de680a92a06deccf
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2021-12-26 18:08:54 +0530

    (updt) removed comments

--- components/Container.js
+++ components/Container.js
@@ -2,19 +2,21 @@ import React from "react";
 import { Popover } from "@headlessui/react";
 import NavBar from "../components/NavBar";
 import Bio from "../components/Bio";
+import BlogHero from "./BlogHero";
+import { useRouter } from "next/router";
+import { routeSchema } from "../public/schemas/routeSchema";
+
+var routeData = routeSchema();
 
 function Container() {
   return (
     <div className="relative overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32">
-          {/* Headlessui popover component */}
           <Popover>
-            {/* Inserting the nav bar component */}
             <NavBar />
           </Popover>
-          {/* Inserting the Bio component */}
-          <Bio />
+          {useRouter().route === routeData.home.route ? <Bio /> : <BlogHero />}
         </div>
       </div>
     </div>
