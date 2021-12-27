7c0450509f3d7ea31cd61e726b9dedada28f587c
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2021-12-27 09:22:21 +0530

    (updt & add) refactored code chunk

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,8 @@
 import { Popover } from '@headlessui/react'
-import { ExclamationIcon } from '@heroicons/react/outline'
 import NavBar from '../components/NavBar'
-import { ctaSchema } from '../public/schemas/ctaSchema'
-import { routeSchema } from '../public/schemas/routeSchema'
-import Link from 'next/link'
-import { ChevronLeftIcon } from '@heroicons/react/outline'
+import NotFoundPage from '../components/NotFoundPage'
 
 function NotFound() {
-  let cta = ctaSchema()
-  let hrefs = routeSchema()
   return (
     <div className="bg-slate-200 dark:bg-slate-900">
       <main>
@@ -19,27 +13,7 @@ function NotFound() {
                 <NavBar />
               </Popover>
             </div>
-            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-              <div className="sm:text-center md:text-center lg:text-center">
-                <div className="text-gray-500">
-                  <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center lg:text-4xl md:text-3xl sm:text-2xl">
-                    <ExclamationIcon className="lg:h-11 lg:w-11 md:h-9 md:w-9 sm:h-8 sm:w-8 h-6 w-6" />
-                    <h1 className="font-bold">Not Found!</h1>
-                  </div>
-                  <p>The page you&apos;re looking for was not found.</p>
-                </div>
-              </div>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
-                <div className="rounded-md shadow">
-                  <Link href={hrefs.home.route}>
-                    <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                      <ChevronLeftIcon className="h-4 w-4" />
-                      {cta.goBackHome}
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </main>
+            <NotFoundPage />
           </div>
         </div>
       </main>
