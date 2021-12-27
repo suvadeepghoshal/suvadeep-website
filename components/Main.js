d590994ecf3f089ea2e2a82384aec936852072ff
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2021-12-27 09:21:57 +0530

    (add) header section

---
+++ components/Main.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Main = () => {
+  return (
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Suvadeep's website" />
+      <meta name="author" content="Suvadeep Ghoshal" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Suvadeep's Website" />
+      <link rel="icon" href="/favicon.png" />
+      <title>Suvadeep Ghoshal - Home</title>
+    </Head>
+  )
+}
+
+export default Main
