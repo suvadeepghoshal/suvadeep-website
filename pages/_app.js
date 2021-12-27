cd7395ec1739826dd42571c1fd08a44ac44be24e
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2021-12-27 09:22:36 +0530

    (updt) global header

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 // This is acting as the main entry point for all the pages in the appplication
 import '../styles/globals.css'
 import '../styles/home.css'
+import Main from '../components/Main'
 
 function MyApp({ Component, pageProps }) {
   const pageColorMaintain = 'bg-slate-200 dark:bg-slate-900'
   const footerDesign = 'text-sm text-center m-auto px-5 py-5 text-gray-400'
   return (
     <div className={pageColorMaintain}>
+      <Main />
       <Component {...pageProps} />
       <footer className={footerDesign}>
         &copy;Suvadeep Ghoshal since 2021. All Rights Reserved.
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
-import Head from 'next/head'
 import Container from '../components/Container'
 
 function Home() {
   return (
     <div className="bg-slate-200 dark:bg-slate-900">
-      <Head>
-        <title>Suvadeep Ghoshal - Home</title>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
       <main>
         <Container />
       </main>
