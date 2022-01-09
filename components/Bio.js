58e5f4dc1978825105215664ebba14f481561ca8
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2022-01-09 13:10:03 +0530

    (updt) changes due to infoSchema restructuring

--- components/Bio.js
+++ components/Bio.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { infoSchema } from '../public/schemas/infoSchema'
-import Link from 'next/link'
-import { ChevronRightIcon } from '@heroicons/react/outline'
 import { ctaSchema } from '../public/schemas/ctaSchema'
 import { routeSchema } from '../public/schemas/routeSchema'
 import PersonalInfo from './PersonalInfo'
@@ -9,8 +7,6 @@ import Hobbies from './Hobbies'
 
 function Bio() {
   let me = infoSchema()
-  let cta = ctaSchema()
-  let hrefs = routeSchema()
   return (
     <>
       <main className="mt-10 max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -24,35 +20,21 @@ function Bio() {
           />
           <h1 className="text-center text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             <span className="block xl:inline dark:text-gray-300">
-              {me.firstName}
+              {me.userInfo.firstName}
             </span>{' '}
             <span className="block text-indigo-600 xl:inline">
-              {me.lastName}
+              {me.userInfo.lastName}
             </span>
           </h1>
           <p className="mt-3 text-base text-center text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-auto font-sans">
-            {me.description}
+            {me.userInfo.description}
           </p>
           <PersonalInfo personal={(info => info?.personal)(me)} />
-          <Hobbies />
-          {/* <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-center">
-            <div className="rounded-md shadow">
-              <Link href={hrefs.posts.route}>
-                <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 dark:bg-indigo-700 dark:hover:bg-indigo-600 md:py-4 md:text-lg md:px-10">
-                  {cta.posts}
-                  <ChevronRightIcon className="h-4 w-4" />
-                </a>
-              </Link>
-            </div>
-            <div className="mt-3 sm:mt-0 sm:ml-3">
-              <Link href={hrefs.portfolio.route}>
-                <a className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 dark:bg-slate-900 dark:hover:bg-slate-800 md:py-4 md:text-lg md:px-10">
-                  {cta.portfolio}
-                  <ChevronRightIcon className="h-4 w-4" />
-                </a>
-              </Link>
-            </div>
-          </div> */}
+          <Hobbies
+            cta={ctaSchema()}
+            routes={routeSchema()}
+            hobbies={(info => info.hobbies)(infoSchema())}
+          />
         </div>
       </main>
     </>
--- pages/404.js
+++ pages/404.js
@@ -13,12 +13,16 @@ function NotFound() {
           <div className="max-w-7xl mx-auto">
             <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32">
               <Popover>
-                <NavBar logo={(info => info.logo)(infoSchema())} />
+                <NavBar logo={(info => info.userInfo.logo)(infoSchema())} />
               </Popover>
             </div>
             <NotFoundPage
-              header={(info => `${info.notFoundHeader}`)(infoSchema())}
-              subHeader={(info => `${info.notFoundSubHeader}`)(infoSchema())}
+              header={(info => `${info.errors[404].notFoundHeader}`)(
+                infoSchema()
+              )}
+              subHeader={(info => `${info.errors[404].notFoundSubHeader}`)(
+                infoSchema()
+              )}
               routes={(() => routeSchema())()}
               cta={(() => ctaSchema())()}
             />
