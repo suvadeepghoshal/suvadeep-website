1662f8ecc5f3f1454929191bb38d5e4da0c0016b
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2022-01-15 17:28:14 +0530

    (updt) getting all post info

--- pages/posts.js
+++ pages/posts.js
@@ -1,15 +1,28 @@
 import Container from '../components/Container'
+import { getSortedPostsData } from '../lib/posts'
 import { routeSchema } from '../public/schemas/routeSchema'
-const Blog = () => {
+const Blog = ({ allPostsData }) => {
   return (
     <>
       <div className="bg-slate-200 dark:bg-slate-900">
         <main>
-          <Container routes={(() => routeSchema())()} />
+          <Container
+            routes={(() => routeSchema())()}
+            allPostsData={allPostsData}
+          />
         </main>
       </div>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
 
 export default Blog
