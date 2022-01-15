2323a037c183930fe9766c041b9e221423ac7a68
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2022-01-15 11:34:50 +0530

    (updt) props for cta in posts

--- components/Posts.js
+++ components/Posts.js
@@ -1,8 +1,17 @@
-import React from "react";
-import BlogHero from "../components/BlogHero";
+import React from 'react'
+import BlogHero from '../components/BlogHero'
+import { ctaSchema } from '../public/schemas/ctaSchema'
+
+export const POST_PER_PAGE = 1 // for now let's consider 1
+
+export async function getStaticProps() {}
 
 function Posts() {
-	return <BlogHero />
+  return (
+    <>
+      <BlogHero goBack={ctaSchema().goBackHome} />
+    </>
+  )
 }
 
-export default Posts;
+export default Posts
