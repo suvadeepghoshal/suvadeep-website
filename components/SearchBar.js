684e004516fe0c9c740b4d1bd04b7183e51d5a4a
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2022-01-15 18:50:34 +0530

    (updt) onchange of val event added

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,12 +1,13 @@
 import { SearchIcon } from '@heroicons/react/outline'
-const SearchBar = function ({ placeHolder }) {
+
+const SearchBar = function ({ placeHolder, changing }) {
   return (
     <>
       <div className="relative max-w-lg">
         <input
           aria-label={placeHolder}
           type="text"
-          // onChange={e => setSearchValue(e.target.value)}
+          onChange={changing}
           placeholder={placeHolder}
           className="block w-full px-4 py-2 text-gray-900 bg-slate-100 border border-gray-300 rounded-md dark:border-gray-900 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-800 dark:text-gray-100"
         />
