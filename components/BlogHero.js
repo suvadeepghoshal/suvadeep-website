c863350a71a380aa54003b8a81b256ffd433673e
Suvadeep Ghoshal <ghoshalsuvadeep594@gmail.com>
2022-01-15 18:11:54 +0530

    (updt) adding header props

--- components/BlogHero.js
+++ components/BlogHero.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
 
-function BlogHero({ goBack }) {
+function BlogHero({ header }) {
   return (
     <>
       <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-        <span className="block dark:text-gray-300">Welcome to</span>
-        <span className="block text-indigo-600">POSTS!</span>
+        <span className="block dark:text-gray-300">{header.first}</span>
+        <span className="block text-indigo-600">{header.second}</span>
       </h2>
     </>
   )
